bed018be763090fd9d5c538446e6940726216cb2
10-4 <>
2024-08-20 15:48:17 +0900

    V1.0.5 게시판 CRUD(mapper,interface 활용)

---
+++ Board/src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,17 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data	// 게터, 세터, toString, equal, 생성자 등을 관여한다.
+public class BoardVO {
+
+	private Long bno ;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updateDate;
+	
+}
